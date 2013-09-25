159c71f324714487bd1ad0d1ac47fd7492e17941
Corwin Lee McKnight <lioneltabre@gmail.com>
2013-09-25 13:18:30 -0700

    IDK

--- core/main.cpp
+++ core/main.cpp
@@ -13,6 +13,5 @@ int main(int argc, char* argv[])
 	ps.loadInfo("sample/bytecode.yml");
 	while(ps.execute())
 	{
-		
 	}
 }
--- includes/bytecode/ybf/YbfParser.h
+++ includes/bytecode/ybf/YbfParser.h
@@ -6,12 +6,14 @@ class YbfParser
 {
 	private:
 		string filepath;
+		uint16_t a,b,c,d;
+		uint8_t memory[255];
 	public:
 		int execute();
 		int start();
 		int stop();
 		void loadInfo(string path);
-	
+		void
 };
 
 #endif
